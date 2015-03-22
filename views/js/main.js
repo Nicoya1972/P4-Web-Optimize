a2dc0c8d14f4053ee701d68bbeddbe57e7035d9d
Nicoya1972 <Nicoya1972@yahoo.com>
2015-03-21 20:58:43 -0700

    update position

--- views/js/main.js
+++ views/js/main.js
@@ -509,11 +509,9 @@ function updatePositions() {
   var items = document.querySelectorAll(".mover");
   var top = (document.body.scrollTop / 1250); //it seems to me that this does not need to be in the loop.
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin(top + (i % 5));
-    var left = -items[i].basicLeft + 1000 * phase + 'px';
-    //using transform
-    items[i].style.transform = "translateX("+left+") translateZ(0)";
-  }
+    var phase = Math.sin(top + (i % 5) *100);
+    items[i].style.transform = 'translateX(' + phase + 'px)';
+  } 
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
