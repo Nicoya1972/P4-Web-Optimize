2285d110c811eacba05ab0af80ac3c145790ee17
Nicoya1972 <Nicoya1972@yahoo.com>
2015-03-31 23:09:12 -0700

    u

--- views/js/main.js
+++ views/js/main.js
@@ -511,7 +511,8 @@ var top = (document.body.scrollTop / 1250);
  
  for (var i = 0; i < items.length; i++) {
     var phase = Math.sin(top + (i % 5));
-    items[i].style.transform = "translateX(" + 100 * phase + "px)";
+    var left = -items[i].basicLeft + 1000 * phase + 'px';
+        items[i].style.transform = "translateX("+left+") translateZ(0)";
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
