1e9e90fd739058c29ceb5a7e8681722e335a58a6
Nicoya1972 <Nicoya1972@yahoo.com>
2015-04-01 22:13:13 -0700

    update position

--- views/js/main.js
+++ views/js/main.js
@@ -500,18 +500,18 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
-
+var items = document.getElementsByClassName('mover');
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-var items = document.getElementsByClassName('mover');
-var topScroll = document.body.scrollTop / 1250;
-var pizzaLength = items.length;
 
- for (var i = 0; i < pizzaLength; i++) {
-    var phase = Math.sin((topScroll) + (i % 5));
+var top = document.body.scrollTop / 1250;
+
+
+ for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin(top + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
