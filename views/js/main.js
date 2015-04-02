9deea26a05edc2b3d8003de24c081c1fc2b1f6ae
Nicoya1972 <Nicoya1972@yahoo.com>
2015-04-01 21:11:43 -0700

    updated

--- views/js/main.js
+++ views/js/main.js
@@ -533,7 +533,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   //decreased # of pizzas displayed http://www.codecademy.com/glossary/javascript#loops
-  for (var i = 30; i--;) {
+  for (var i = 0; i < 31; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
