059331ac9e2eb575416f9af1123ca8fd35b6a7cc
Nicoya1972 <Nicoya1972@yahoo.com>
2015-04-02 22:53:25 -0700

    push

--- views/js/main.js
+++ views/js/main.js
@@ -511,7 +511,7 @@ function updatePositions() {
   for (var i = 0; i < pizzas.length; i++) {
     var phase = Math.sin(scrollTop + (i % 5));
     var PizzaLeft = pizzas[i].basicLeft + 1000 * phase + 'px';
-     items[i].style.transform = "translateX("+PizzaLeft+") translateZ(0)";
+     pizzas[i].style.transform = "translateX("+PizzaLeft+") translateZ(0)";
     
   }
 
